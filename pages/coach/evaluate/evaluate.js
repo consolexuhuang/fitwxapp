a796df9ac7452088222fd232dc8bbe92eb0055cb
consolexuhuang <1758050394@qq.com>
2019-06-17 11:04:09 +0800

    bug修复

--- pages/coach/evaluate/evaluate.js
+++ pages/coach/evaluate/evaluate.js
@@ -41,8 +41,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if (true) {
-      // this.setData({ orderNum: options.orderNum})
+    if (options.orderNum) {
+      this.setData({ orderNum: options.orderNum})
       let data = {
         orderNum: options.orderNum || '',
       }
