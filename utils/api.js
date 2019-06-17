672a83af93e058b8bfa7ef4e8d662bcffd44a3a0
consolexuhuang <1758050394@qq.com>
2019-06-17 13:05:48 +0800

    更新code

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
-//  const API_URI = 'https://fit.jlife.vip/wx/api/'
-const API_URI = 'https://dev.jlife.vip/wx/api/'
+ const API_URI = 'https://fit.jlife.vip/wx/api/'
+// const API_URI = 'https://dev.jlife.vip/wx/api/'
 import Store from './store.js'
 function request(path, data, method) {
   return new Promise(function(resolve, reject) {
@@ -16,7 +16,7 @@ function request(path, data, method) {
       success: function(res) {
         console.log('noFind',res)
         if (getApp().globalData.redirectToState){
-          if (res.code === 0) {
+          if (res.data.code === 0){
             resolve(res.data)
             getApp().globalData.redirectToState = true
           }
