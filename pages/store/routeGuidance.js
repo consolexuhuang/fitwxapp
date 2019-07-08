7760e54c1833098ba003921cd0d19eba1f25c843
consolexuhuang <1758050394@qq.com>
2019-07-08 11:10:39 +0800

    更新线路指引分享

--- pages/store/routeGuidance.js
+++ pages/store/routeGuidance.js
@@ -1,10 +1,14 @@
+const app = getApp();
+const api = app.api
+const store = getApp().store;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    webUrl:''
+    webUrl:'',
   },
 
   /**
@@ -29,39 +33,20 @@ Page({
   onShow: function () {
     
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+    return {
+      title: '线路指引',
+      path: `/pages/store/routeGuidance?webUrl=${this.data.webUrl}`,
+      // imageUrl: this.data.picList[0],
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    }
   }
 })
