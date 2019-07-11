b366cc8cb01c1655775dccc9a149cbabef5500f3
huxiaoxiao <huxiaolifox01@163.com>
2019-07-11 17:55:44 +0800

    照片静态页面

---
+++ pages/coursePhoto/coursePhoto.js
@@ -0,0 +1,77 @@
+// pages/coursePhoto/coursePhoto.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navbarData: {
+      title: '照片',
+      showCapsule: 1,
+      isShowBackHome: true,
+      titleColor: "#000",
+      tab_topBackground: '#fff'
+    },
+    marginTopBar: getApp().globalData.tab_height * 2 + 20,
+    boxWidthScale:'686',//盒子宽度
+    infoPadding:'23',//信息行的间距
+    qrCodeWidthScale:'82',//二维码宽度
+    qrCodeHeightScale:'82',//二维码高度
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
