0eff558ebfee44f42f20788ba38d51f3bd90a22b
huxiaoxiao <huxiaolifox01@163.com>
2019-09-12 18:21:51 +0800

    卡片

--- components/shareMain/shareMain.js
+++ components/shareMain/shareMain.js
@@ -2,6 +2,8 @@
 const app = getApp();
 const api = app.api;
 const util = require('../../utils/util.js');
+const ui = require('../../utils/ui.js');
+let firstObservers = true;//是否第一次监听
 Component({
   /**
    * 组件的属性列表
@@ -14,7 +16,24 @@ Component({
       type: Boolean,
       value: false
     }
-  },  
+  },
+  /* 监听 */
+  observers: {
+    'isShow':function(val){
+      if(val){
+        ui.hideLoading();
+      }
+    },
+    'cardData': function (val) {
+      console.log('000')
+      //生成图片        第二个memberHeadImg要改为二维码
+      if (val.pic && val.memberHeadImg && val.memberHeadImg && firstObservers) {
+        console.log('66666666666666666')
+        this.generateCardPic();
+        firstObservers=false;
+      }
+    }
+  },
   /**
    * 组件的初始数据
    */
@@ -26,34 +45,20 @@ Component({
     sPicHeight: 302, //源图片高度
     picHeight: 450, //绘制图片高度450
     borderRadius: 32, //矩形背景圆角
-    picBorderRadius:60, //矩形背景圆角
-    courseNameFontSizeScale: 15,//课程标题字体大小比例，课程名字体大小 width/courseNameFontSizeScale
-    courseNameLineHeight:40,//一行标题的高度
-    infoSpaceLeft:60,//3行信息距离左侧距离
-    infoSpaceTop:30,//每行信息距离上一行距离
-    iconScale: 18,//信息图标大小比例，大小 width/iconScale
-    infoRowHeight:60,//每行信息高度
-    headImgScale: 6.5,//头像大小比例，大小 width/headImgScal
-    tipText:'分享课程，长按查看',
-    qrCodeWidth:88,//二维码宽度
-    addressWidth:0,//真实地址宽度
-    addressRowMaxWidth: 550 - 60 * 3,//可装地址的行宽
-    addressIsTwoRow:false,//地址是否是2行
-  },
-  /* 监听 */
-  observers:{
-    'cardData.address':function(val){     
-
-      //获取地址宽度，用来判断地址是一行还是2行，给canvas赋值对应的高度
-      let address = val;
-      /* //测试
-      let address = '后面的参数是实际的url，需要先encode一下你试一下这个接口，可以做跳转'; */
-      console.log('observers000')
-      /* if (address){
-        console.log('observers')
-        this.calAddressWidth(address);
-      } */
-    }
+    picBorderRadius: 60, //矩形背景圆角
+    courseNameFontSizeScale: 15, //课程标题字体大小比例，课程名字体大小 width/courseNameFontSizeScale
+    courseNameLineHeight: 40, //一行标题的高度
+    infoSpaceLeft: 60, //3行信息距离左侧距离
+    infoSpaceTop: 30, //每行信息距离上一行距离
+    iconScale: 18, //信息图标大小比例，大小 width/iconScale
+    infoRowHeight: 60, //每行信息高度
+    headImgScale: 6.5, //头像大小比例，大小 width/headImgScal
+    tipText: '分享课程，长按查看',
+    qrCodeWidth: 88, //二维码宽度
+    addressWidth: 0, //真实地址宽度
+    addressRowMaxWidth: 550 - 60 * 3, //可装地址的行宽
+    addressIsTwoRow: false, //地址是否是2行
+    generateFilePath:'',//canvas生成的图片
   },
   lifetimes: {
     ready: function() {
@@ -71,26 +76,43 @@ Component({
    */
   methods: {
     init() {
-      
     },
-   
+
     //关闭弹层
     clickClose() {
       this.triggerEvent('closecard')
     },
 
-   
-
     //保存卡片
     saveCard() {
+      //loading
+      ui.showLoadingMask();
+      let generateFilePath = this.data.generateFilePath;
+      if (generateFilePath){
+        this.saveImageToPhotosAlbum(generateFilePath)
+        .then(()=>{
+          ui.hideLoading()
+        });
+      }else{
+        this.generateCardPic()
+        .then(()=>{
+          return this.saveImageToPhotosAlbum(generateFilePath);
+        })
+        .then(() => {
+          ui.hideLoading()
+        });
+      }
+    },
+
+    //生成卡片图片
+    generateCardPic() {
       /* //测试
       this.setData({
         'cardData.address':'远程图片转本地图片后面只需要把第三个参数改为二维码就行了',
          'cardData.courseName':'barbie我们只能设置文本的最大宽度，这就产生一定的了问题'
       }) */
-
       //远程图片转本地图片（banner、头像、二维码）     后面只需要把第三个参数改为二维码就行了
-      Promise.all([this.remoteToLocal(this.data.cardData.pic), this.remoteToLocal(`${api.API_URI}redirect?url=${encodeURI(this.data.cardData.memberHeadImg)}`), this.remoteToLocal(`${api.API_URI}redirect?url=${encodeURI(this.data.cardData.memberHeadImg)}`), this.calAddressWidth(this.data.cardData.address)])
+    return Promise.all([this.remoteToLocal(this.data.cardData.pic), this.remoteToLocal(`${api.API_URI}redirect?url=${encodeURI(this.data.cardData.memberHeadImg)}`), this.remoteToLocal(`${api.API_URI}redirect?url=${encodeURI(this.data.cardData.memberHeadImg)}`), this.calAddressWidth(this.data.cardData.address)])
         .then((resArrImg) => {
           //banner
           let pic = resArrImg[0];
@@ -103,9 +125,9 @@ Component({
           this.canvasToPic()
         })
         .catch((err) => {
+          ui.showToast(err);
           console.log(err)
         })
-
     },
 
     //绘制canvas
@@ -114,40 +136,40 @@ Component({
       console.log(this.data)
       let canvasWidth = this.data.width;
       let canvasHeight = this.data.height;
-      let borderRadius = this.data.borderRadius;      
+      let borderRadius = this.data.borderRadius;
       let sPicWidth = this.data.sPicWidth; //源图片宽度
       let sPicHeight = this.data.sPicHeight; //源图片高度
       let picHeight = this.data.picHeight;
       let picBorderRadius = this.data.picBorderRadius;
       let courseNameFontSizeScale = this.data.courseNameFontSizeScale;
-      let courseNameWidth = canvasWidth - picBorderRadius*2;//课程标题宽度
+      let courseNameWidth = canvasWidth - picBorderRadius * 2; //课程标题宽度
       let courseNameLineHeight = this.data.courseNameLineHeight;
-      let infoSpaceLeft = this.data.infoSpaceLeft;//3行信息距离左侧距离
-      let infoSpaceTop = this.data.infoSpaceTop;//每行信息距离上一行距离
+      let infoSpaceLeft = this.data.infoSpaceLeft; //3行信息距离左侧距离
+      let infoSpaceTop = this.data.infoSpaceTop; //每行信息距离上一行距离
       let iconScale = this.data.iconScale;
       let infoRowHeight = this.data.infoRowHeight;
       let headImgScale = this.data.headImgScale;
       let qrCodeWidth = this.data.qrCodeWidth;
-      let addressWidth = this.data.addressWidth;//真实地址宽度
-      let addressRowMaxWidth = this.data.addressRowMaxWidth;//可装地址的行宽
+      let addressWidth = this.data.addressWidth; //真实地址宽度
+      let addressRowMaxWidth = this.data.addressRowMaxWidth; //可装地址的行宽
       let addressIsTwoRow = this.data.addressIsTwoRow;
-      let addRowHeight = addressIsTwoRow? infoSpaceTop : 0;
+      let addRowHeight = addressIsTwoRow ? infoSpaceTop : 0;
 
       /* canvas draw */
-      let ctx = wx.createCanvasContext('cardCanvas', this);  
+      let ctx = wx.createCanvasContext('cardCanvas', this);
       /* 绘制背景圆角白色矩形 */
       //绘制圆角--左上角
-      ctx.arc(borderRadius, borderRadius, borderRadius, 0, 2 * Math.PI);      
+      ctx.arc(borderRadius, borderRadius, borderRadius, 0, 2 * Math.PI);
       //关闭
       ctx.closePath()
       //绘制圆角--右上角
-      ctx.arc( canvasWidth - borderRadius, borderRadius, borderRadius, 0, 2 * Math.PI);   
+      ctx.arc(canvasWidth - borderRadius, borderRadius, borderRadius, 0, 2 * Math.PI);
       //关闭
-      ctx.closePath()   
+      ctx.closePath()
       //绘制圆角--左下角
-      ctx.arc(borderRadius, canvasHeight - borderRadius, borderRadius, 0, 2 * Math.PI);     
+      ctx.arc(borderRadius, canvasHeight - borderRadius, borderRadius, 0, 2 * Math.PI);
       //关闭
-      ctx.closePath() 
+      ctx.closePath()
       //绘制圆角--右下角
       ctx.arc(canvasWidth - borderRadius, canvasHeight - borderRadius, borderRadius, 0, 2 * Math.PI);
       //关闭
@@ -163,12 +185,12 @@ Component({
       ctx.lineTo(borderRadius, canvasHeight)
       ctx.lineTo(0, canvasHeight - borderRadius)
       // 左边框
-      ctx.lineTo(0, borderRadius)      
+      ctx.lineTo(0, borderRadius)
       ctx.lineTo(borderRadius, 0)
       //关闭
-      ctx.closePath()      
+      ctx.closePath()
       ctx.setFillStyle('#fff');
-      ctx.fill(); 
+      ctx.fill();
 
       /* 绘制顶部渐变圆角图片 */
       ctx.save();
@@ -206,37 +228,37 @@ Component({
       //关闭
       ctx.closePath();
       ctx.setFillStyle('#fff');
-      ctx.fill(); 
+      ctx.fill();
       //绘制图片
       ctx.clip();
-      ctx.drawImage(pic, 0, 0, sPicWidth, sPicHeight, 0, 0, canvasWidth,picHeight);
+      ctx.drawImage(pic, 0, 0, sPicWidth, sPicHeight, 0, 0, canvasWidth, picHeight);
       //渐变色
       let grd = ctx.createLinearGradient(canvasWidth / 2, 0, canvasWidth / 2, picHeight);
       grd.addColorStop(0, 'rgba(0,0,0,0)')
       grd.addColorStop(0.5, 'rgba(0,0,0,0)')
-      grd.addColorStop(1,'rgba(0,0,0,1)')
-      ctx.setFillStyle(grd) 
+      grd.addColorStop(1, 'rgba(0,0,0,1)')
+      ctx.setFillStyle(grd)
       ctx.fill();
       ctx.restore();
       //课程标题
       ctx.setFontSize(canvasWidth / courseNameFontSizeScale);
       ctx.setFillStyle('#fff');
       let courseName = this.data.cardData.courseName;
-      let row = this.textRow(ctx,courseName, courseNameWidth); 
+      let row = this.textRow(ctx, courseName, courseNameWidth);
       //1行
-      if (row.length==1){
+      if (row.length == 1) {
         ctx.fillText(row[0], picBorderRadius, picHeight - courseNameLineHeight);
       }
       //2行
-      else{
+      else {
         ctx.fillText(row[0], picBorderRadius, picHeight - courseNameLineHeight * 2 - 10);
         ctx.fillText(row[1], picBorderRadius, picHeight - courseNameLineHeight);
       }
-      
+
       //时间
       let beginDate = this.data.cardData.beginDate;
       let beginTime = this.data.cardData.beginTime;
-      let date = beginDate ? util.formatTime(beginDate).replace(/\//g,'-').split(' ')[0] : '';
+      let date = beginDate ? util.formatTime(beginDate).replace(/\//g, '-').split(' ')[0] : '';
       let time = beginTime ? util.formatTime3(beginTime).replace(/\//g, '-').split(' ')[0] : '';
       let dateTime = date + ' ' + time;
       //绘制图标
@@ -244,30 +266,30 @@ Component({
       //绘制文本
       ctx.setFontSize(28);
       ctx.setFillStyle('#999999');
-      ctx.fillText(dateTime, infoSpaceLeft + canvasWidth / iconScale + infoSpaceTop, picHeight + infoSpaceTop+25);
+      ctx.fillText(dateTime, infoSpaceLeft + canvasWidth / iconScale + infoSpaceTop, picHeight + infoSpaceTop + 25);
 
       //地址
       //绘制图标
       ctx.drawImage('/images/icon/address.png', 0, 0, 56, 56, infoSpaceLeft, picHeight + infoSpaceTop + infoRowHeight, canvasWidth / iconScale, canvasWidth / iconScale);
       //绘制文本
       ctx.setFontSize(28);
-      ctx.setFillStyle('#999999');  
+      ctx.setFillStyle('#999999');
       let address = this.data.cardData.address;
       let row_address = this.textRow(ctx, address, addressRowMaxWidth);
       //1行
-      if (row_address.length==1){
+      if (row_address.length == 1) {
         ctx.fillText(row_address[0], infoSpaceLeft + canvasWidth / iconScale + infoSpaceTop, picHeight + infoSpaceTop + infoRowHeight + 25);
       }
       //2行
-      else{
+      else {
         ctx.fillText(row_address[0], infoSpaceLeft + canvasWidth / iconScale + infoSpaceTop, picHeight + infoSpaceTop + infoRowHeight + 25);
         ctx.fillText(row_address[1], infoSpaceLeft + canvasWidth / iconScale + infoSpaceTop, picHeight + infoSpaceTop + infoRowHeight + 60);
       }
-      
+
 
       console.log('addRowHeight')
       console.log(addRowHeight)
-      
+
       //教练
       //绘制图标
       ctx.drawImage('/images/icon/people.png', 0, 0, 56, 56, infoSpaceLeft, picHeight + infoSpaceTop + infoRowHeight * 2 + addRowHeight, canvasWidth / iconScale, canvasWidth / iconScale);
@@ -280,12 +302,12 @@ Component({
       //会员头像
       ctx.save()
       ctx.beginPath()
-      let headImgX = infoSpaceLeft+20;
+      let headImgX = infoSpaceLeft + 20;
       let headImgY = picHeight + infoSpaceTop * 3 + infoRowHeight * 3 + addRowHeight;
       let headImgWidth = canvasWidth / headImgScale;
       let headImgR = headImgWidth / 2;
-      ctx.arc(headImgX, headImgY, headImgR, 0, 2*Math.PI);
-      ctx.clip()      
+      ctx.arc(headImgX, headImgY, headImgR, 0, 2 * Math.PI);
+      ctx.clip()
       ctx.drawImage(memberHeadImg, 0, 0, 132, 132, headImgX - headImgR, headImgY - headImgR, headImgWidth, headImgWidth)
       ctx.restore();
       //会员昵称
@@ -307,29 +329,29 @@ Component({
         }, 1000))
       }).catch((err) => {
         console.log('绘制canvas错误！')
-        reject(err)
+        reject('绘制canvas错误！')
       })
     },
 
-   //给文字分组
-    textRow(ctx,textString,stringWidth){
-     /* //测试
-     let textString = 'barbie我们只能设置文本的最大宽度，这就产生一定的了问题'; */
-     let chr = textString ? textString.split("") : [];
-     let temp = "";
-     let row = [];
-     for (let a = 0; a < chr.length; a++) {
-       if (ctx.measureText(temp).width < stringWidth) {
-         temp += chr[a]
-       } else {
-         a--;
-         row.push(temp);
-         temp = "";
-       }
-     }
-     row.push(temp);
-     return row;
-   },
+    //给文字分组
+    textRow(ctx, textString, stringWidth) {
+      /* //测试
+      let textString = 'barbie我们只能设置文本的最大宽度，这就产生一定的了问题'; */
+      let chr = textString ? textString.split("") : [];
+      let temp = "";
+      let row = [];
+      for (let a = 0; a < chr.length; a++) {
+        if (ctx.measureText(temp).width < stringWidth) {
+          temp += chr[a]
+        } else {
+          a--;
+          row.push(temp);
+          temp = "";
+        }
+      }
+      row.push(temp);
+      return row;
+    },
     //计算地址的宽度
     calAddressWidth(address) {
       return new Promise((resolve, reject) => {
@@ -373,9 +395,13 @@ Component({
           console.log('canvasToTempFilePath')
           console.log(res)
           let tempFilePath = res.tempFilePath;
+          this.setData({
+            generateFilePath: tempFilePath
+          })
+          /* //测试
           wx.redirectTo({
             url: `/pages/test/test?imgUrl=${tempFilePath}&width=${this.data.width}&height=${this.data.height}`,
-          })
+          }) */
         },
         fail: (err) => {
           console.log('图片保存失败！')
@@ -394,11 +420,30 @@ Component({
             resolve(res.path);
           },
           fail: (err) => {
-            console.log('远程图片转本地图片错误！')
-            reject(err)
+            console.error(err)
+            reject('远程图片转本地图片错误！')
           }
         })
       })
     },
+   //保存图片到系统相册
+    saveImageToPhotosAlbum(url){
+      console.log('保存图片到系统相册')
+      console.log(url)
+     return new Promise((resolve,reject)=>{
+       wx.saveImageToPhotosAlbum({
+         filePath: url,
+         success: () => {
+           ui.showToast('卡片已保存到相册')
+           resolve();
+         },
+         fail: (err) => {
+           console.error(err)
+           ui.showToast('卡片保存失败')
+           reject('卡片保存失败')
+         }
+       })
+     })  
+    }
   }
 })
--- pages/course/courseDetail.js
+++ pages/course/courseDetail.js
@@ -1,7 +1,8 @@
 // pages/course/courseDetail.js
 const app = getApp();
 const api = app.api
-const format = require('../../utils/util.js')
+const format = require('../../utils/util.js');
+const ui = require('../../utils/ui.js');
 const store = app.store;
 
 Page({
@@ -44,7 +45,7 @@ Page({
       memberNickName:'',
       qrCode:''
     },//保存卡片需要的信息
-    isShowCard:true,//是否显示卡片内容
+    isShowCard:false,//是否显示卡片内容
 
     statementContent:`<p>1、参与Justin&Julie健身服务的用户，具有完全的民事行为能力，同意遵守Justin&Julie的相关管理规章制度，已接受Justin&Julie的相关服务协议，并已知晓有关的健身规则与警示，承诺遵守Justin&Julie的相关健身规则与警示规定。</p>
       <p>2、Justin&Julie员工及教练不提供任何形式的体检服务，Justin&Julie员工及教练对用户身体情况的任何询问、了解和建议都不构成本公司对用户身体状况是否符合任意健身课程和产品要求的承诺及保证。在确认本声明前，用户应自行到医疗机构进行体检，了解自身身体情况，以确保用户具备参与Justin&Julie健身产品的身体条件，且没有任何不宜运动的疾病、损伤和其他缺陷。因用户自身的任何疾病、损伤或其他缺陷导致用户在接受服务时发生任何损害的，Justin&Julie不承担任何法律责任。</p>
@@ -174,9 +175,16 @@ Page({
   },
   //点我分享
   handleShare(){
-    this.setData({
-      isShowCard:true
+    ui.showLoading();
+    //版本校验
+    app.compareVersionPromise('2.6.1').then((res) => {
+      if (res == 0) {
+        this.setData({
+          isShowCard: true
+        })
+      }
     })
+    
   },
   //关闭分享
   closecard(){
