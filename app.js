10b82e093ea9cf54354e84ffe731c113ded2eafb
huxiaoxiao <huxiaolifox01@163.com>
2019-10-28 18:40:27 +0800

    默认更新版本

--- app.js
+++ app.js
@@ -11,25 +11,18 @@ App({
     this.store = Store;
     this.worker = worker;
     this.globalData.scene = options.scene;
+    
 
-    /* //版本更新
+    //版本更新
     if (wx.canIUse('getUpdateManager')) {
       const updateManager = wx.getUpdateManager()
       updateManager.onCheckForUpdate(function (res) {
         // 请求完新版本信息的回调
         if (res.hasUpdate) {
           updateManager.onUpdateReady(function () {
-            wx.showModal({
-              title: '更新提示',
-              content: '新版本已经准备好，是否重启应用？',
-              showCancel: false,
-              success: function (res) {
-                if (res.confirm) {
-                  // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-                  updateManager.applyUpdate()
-                }
-              }
-            })
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+            console.log('更新成功')
           })
           updateManager.onUpdateFailed(function () {
             // 新的版本下载失败
@@ -46,7 +39,7 @@ App({
         title: '提示',
         content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
       })
-    } */
+    }
 
 
     //获取不同设备高度
