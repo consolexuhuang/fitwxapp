36b26845ffbbad0911f011cee072dc3dbd9a734a
consolexuhuang <1758050394@qq.com>
2019-12-24 18:40:02 +0800

    更新样式和顺序

--- pages/subPackages_report/report/index.js
+++ pages/subPackages_report/report/index.js
@@ -1,4 +1,5 @@
 // pages/subPackages_report/report/index.js
+const api = getApp().api;
 Page({
 
   /**
@@ -10,13 +11,45 @@ Page({
     isStartState: false,
     jurisdictionSmallState: false,
     isLoadOverState: true,
+    userYearReport:''
   },
+  //配置邀请二维码
+  getCodeConfig() {
+    return new Promise(resolve => {
+      let data = {
+        scene: store.getItem('userData').id,
+        liteType: 'main',
+        page: 'pages/report/index'
+      }
+      console.log(util.formatUrlParams(`${getApp().globalData.API_URI}getLiteQrcode`, data))
+      // this.setData({
+      //   invitedcodeUrl: util.formatUrlParams(`${getApp().globalData.API_URI}getLiteQrcode`, data)
+      // })
+      resolve()
+    })
+  },
+  // 生成年度账单
+  getYearReport(){
+    api.post('v2/member/generateYearReport2019').then(res => {
+      console.log('2019账单',res)
+      this.setData({ userYearReport: res.msg})
+    })
+  },
+  // getreport(){
+  //   let data = {
+  //     id: "1209343800225435648"
+  //   }
+  //   api.post('v2/member/getYearReport2019', data).then(res => {
+  //     console.log('2019账单2', res)
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     getApp().checkSessionFun().then(() => {
-
+      this.getYearReport()
+      // this.getreport()
     })
   },
 
